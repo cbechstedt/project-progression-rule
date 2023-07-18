0095f198b8d5b3b7526c2ef4e64ff0185ebd7074
Christian <chrisnebech@hotmail.com>
2023-07-18 15:56:50 -0300

    requisito 2 e 3

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -16,12 +16,38 @@ public class App {
     System.out.println("Digite a quantidade de atividades para cadastrar:");
     int quantity = Integer.parseInt(scanner.nextLine());
 
+    String activityName;
+    int activityWeight;
+    int activityScore;
+    int activityFinalScore;
+    int sumFinalScore = 0;
+    int sumFinalWeights = 0;
+
     for (int i = 1; i <= quantity; i++) {
       System.out.println("Digite o nome da atividade " + i + ":");
-      String activityName = scanner.nextLine();
+      activityName = scanner.nextLine();
 
       System.out.println("Digite o peso da atividade " + i + ":");
-      int activityWeight = Integer.parseInt(scanner.nextLine());
+      activityWeight = Integer.parseInt(scanner.nextLine());
+
+      System.out.println("Digite a nota obtida para " + activityName + ":");
+      activityScore = Integer.parseInt(scanner.nextLine());
+
+      activityFinalScore = activityScore * activityWeight;
+
+      sumFinalScore = sumFinalScore + activityFinalScore;
+      sumFinalWeights = sumFinalWeights + activityWeight;
+    }
+
+    double finalScore = sumFinalScore / sumFinalWeights;
+    if (finalScore < 85) {
+      System.out.println(
+          "Lamentamos informar que, com base na sua pontuação alcançada neste período, "
+              + finalScore
+              + "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
+    } else {
+      System.out.println("Parabéns! Você alcançou " + finalScore
+          + "%! E temos o prazer de informar que você obteve aprovação!");
     }
   }
 }
